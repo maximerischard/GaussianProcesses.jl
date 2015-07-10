396d4c40d5b3ecbca56c08f975001292b6c8995b
Christopher Nemeth <christopher.nemeth@hotmail.com>
2015-07-10 15:18:42 +0100

    fixed derivatives for marginal log-likelihood

--- src/inference/laplace.jl
+++ src/inference/laplace.jl
@@ -4,46 +4,48 @@ function laplace!(gp::logisticGP)
 
     m = meanf(gp.m,gp.x)                      # Evaluate the mean function
     k = crossKern(gp.x,gp.k)                  # Evaluate the kernel
-    alpha = zeros(gp.nobsv)                 # Starting value for α, NEEDS TO BE CHECKED
+    gp.alpha = zeros(gp.nobsv)                 # Starting value for α, NEEDS TO BE CHECKED
 
-    #Psi = negative log-posterior as defined in Rasmussen and Williams (2006) p.42
+    #Psi = log-posterior as defined in Rasmussen and Williams (2006) p.42
     function psi(alpha::Vector{Float64})             
         f = k*alpha+m;
         lp = like(gp.y,f)
-        psi = dot(alpha, (f-m))/2.0 - sum(lp) #up to a normalising constant
-        return psi           
+        psi = -dot(alpha, (f-m))/2.0 + sum(lp) #up to a normalising constant
+        return -psi           
     end
 
     #1st derivative of Psi    
     function dPsi!(alpha::Vector{Float64}, grad::Vector{Float64})
         f = k*alpha+m;
         dlp = grad1_likef(gp.y,f) 
-        dpsi = k*(alpha-dlp)
-        grad[:] = dpsi
+      #  dpsi = k*(alpha-dlp)
+        dpsi = dlp - alpha
+        grad[:] = -dpsi
     end
 
     #2nd derivative of Psi
     function d2Psi!(alpha::Vector{Float64}, hess::Matrix{Float64})
         f = k*alpha+m;
         d2lp = grad2_likef(gp.y,f)
-        dpsi2 = -diagm(d2lp) - inv(k)
-        hess[:,:] = dpsi2
+        dpsi2 = diagm(d2lp) - inv(k)
+        hess[:,:] = -dpsi2
     end
 
 #func = TwiceDifferentiableFunction(psi,dPsi!,d2Psi!)
-    res = optimize(psi, dPsi!, alpha)
-    #res = optimize(psi, dPsi!, d2Psi!, alpha, show_trace=true, method=:newton)
+    #res = optimize(psi, dPsi!, gp.alpha,show_trace=true)
+    res = optimize(psi, dPsi!, d2Psi!, gp.alpha, show_trace=true, method=:newton)
     #res = optimize(psi, alpha; method = :nelder_mead)   #NEED TO CREATE OPTIMIZATION FUNCTION TO MAXIMISE PHI, REQUIRE 1ST AND 2ND DERIVATIVES OF PHI
 
-    gp.alpha = res.minimum    
+        
+    gp.alpha = res.minimum  
 
     #Calculate the posterior    
-    f = k*alpha+m;                                  # compute latent function values
+    f = k*gp.alpha+m;                                  # compute latent function values
     lp = like(gp.y,f); W = -grad2_likef(gp.y,f);
     sW = sqrt(abs(W)).*(W./abs(W));                   #Use W./abs(W) to preserve the sign
 
     L = PDMat(eye(gp.nobsv)+sW*sW'.*k);                 
-    gp.mLL = -dot(gp.alpha,f-m)/2.0 - sum(log(diag(L))+lp);   # ..(f-m)/2 -lp +ln|B|/2 negative marginal log-likelihood
+    gp.mLL = -dot(gp.alpha,f-m)/2.0 - sum(log(diag(L))+lp);   #marginal log-likelihood
 end
 
 
@@ -72,7 +74,7 @@ function update_laplace_and_dmll!(gp::logisticGP,mean::Bool=true, kern::Bool=tru
     if mean
         Mgrads = grad_stack(gp.x, gp.m)                              # [dM/dθᵢ]    
         for i in 1:num_params(gp.m)
-            gp.dmLL[i] = -dot(gp.alpha,Mgrads[:,i]) - dot(dfhat,(Mgrads[:,i]-k*(Z*Mgrads[:,i])));         
+            gp.dmLL[i] = dot(gp.alpha,Mgrads[:,i]) + dot(dfhat,(Mgrads[:,i]-k*(Z*Mgrads[:,i])));         
         end
     end
 
@@ -81,7 +83,7 @@ function update_laplace_and_dmll!(gp::logisticGP,mean::Bool=true, kern::Bool=tru
         Kgrads = grad_stack(gp.x, gp.k)                              # [dK/dθᵢ]    
         for i in 1:num_params(gp.k)
             b = Kgrads[:,:,i]*grad1_likef(gp.y,f) ;       # b-K*(Z*b) = inv(eye(nobsv)+K*diag(W))*b
-            gp.dmLL[i+mean*num_params(gp.m)] = sum(sum(Z.*Kgrads[:,:,i]))/2.0-dot(gp.alpha,Kgrads[:,:,i]*gp.alpha)/2.0-dot(dfhat,(b-k*(Z*b)));  
+            gp.dmLL[i+mean*num_params(gp.m)] = -sum(sum(Z.*Kgrads[:,:,i]))/2.0 + dot(gp.alpha,Kgrads[:,:,i]*gp.alpha)/2.0 + dot(dfhat,(b-k*(Z*b)));  
         end
    end
 end
