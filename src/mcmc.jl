5fa9445609516d9c8944b97f9ac214a7fb35fe4e
nemethis <git@github.com:nemethis/GaussianProcesses.git>
2016-05-12 23:17:54 +0100

    added unfinished implementation of adding a user specified prior to the posterior in the mcmc

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -13,25 +13,34 @@ function mcmc(gp::GP;
               sampler::Lora.MCSampler=Lora.MH(ones(length(get_params(gp)))),
               mcrange::Lora.BasicMCRange=BasicMCRange(nsteps=5000, burnin=1000))
 
-
+ 
     store = get_params(gp) #store original parameters
     npara = length(store)  #number of parameters
-    
+
     prior = Distributions.MvNormal(zeros(npara),100*eye(npara)) #default prior
+    # prior=Array(Distribution,npara)
+    # if eltype(prior)==Distributions.Distribution
+    # logprior(prior,hyp::Vector{Float64}) = sum([logpdf(prior[i],hyp[i]) for i=1:npara])
+    # gradlogprior(prior,hyp::Vector{Float64}) = sum([gradlogpdf(prior[i],hyp[i]) for i=1:npara])
+    # else
+    #  logprior(prior,hyp::Vector{Float64}) = logpdf(prior,hyp)
+    #  gradlogprior(prior,hyp::Vector{Float64}) = sum(gradlogpdf(prior,hyp))
+    # end
+
     
-    function mll(hyp::Vector{Float64})  #log-target
+    function lpost(hyp::Vector{Float64})  #log-target
         set_params!(gp, hyp)
         update_mll!(gp)
         return gp.mLL + logpdf(prior,hyp)
     end
     
-    function dmll(hyp::Vector{Float64}) #gradient of the log-target
+    function dlogpost(hyp::Vector{Float64}) #gradient of the log-target
         set_params!(gp, hyp)
         update_mll_and_dmll!(gp)
-        return gp.dmLL + sum(gradlogpdf(prior,hyp))
+        return gp.dmLL + sum(gradlogpdf(prior,hyp)) 
     end
     starting = Dict(:p=>start)
-    q = BasicContMuvParameter(:p, logtarget=mll,gradlogtarget=dmll) 
+    q = BasicContMuvParameter(:p, logtarget=lpost,gradlogtarget=dlogpost) 
     model = likelihood_model(q, false)                               #set-up the model
     tune = VanillaMCTuner(period=mcrange.burnin)                     #set length of tuning (default to burnin length)
     job = BasicMCJob(model, sampler, mcrange, starting,tuner=tune)   #set-up MCMC job
